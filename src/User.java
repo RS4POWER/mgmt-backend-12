d02d45dadc887c0ceb935271a408265505b90e4d
Onigiri <kethrinlie@yahoo.com>
2022-02-24 21:15:54 +0200

    User Class

--- src/User.java
+++ src/User.java
@@ -1,2 +1,19 @@
+class NameException extends Exception{
+    public NameException(String message){
+        super(message);
+    }
+}
+
 public class User {
+    String firstName, lastName, email, roles;
+
+    public User(String firstName,String lastName, String email,String roles) throws NameException{
+        this.firstName = firstName;
+        this.lastName = lastName;
+        if (email.contains("@"))
+            this.email = email;
+        else throw new NameException("Email invalid");
+        this.roles = roles;
+
+    }
 }
